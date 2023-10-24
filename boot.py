084dc028d2fcee5cecd766f87af334edca42e5d0
HaXr <haxr@outlook.dk>
2023-10-24 10:49:56 +0200

    Initial commit
    Yoinked code from https://github.com/CDBSchoolStuff/MicroPython-Stuff/tree/ILS8-Fjernafl%C3%A6sning-System

---
+++ boot.py
@@ -0,0 +1,9 @@
+# Ukommenter, hvis der arbejdes med MQTT
+# --------------------------------------
+
+import sys
+
+sys.path.reverse()
+print("\n\n\nESP32 starter op")
+
+# --------------------------------------
